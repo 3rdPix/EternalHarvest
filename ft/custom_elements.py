3a78d610f4a694e0610ecbd163b291d1ed79c957
3rdPix <xireeell@my.com>
2022-11-15 06:13:17 -0300

    created grid view for mobs

--- ft/custom_elements.py
+++ ft/custom_elements.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QLabel, QGroupBox, QFrame
+from PyQt5.QtWidgets import QLabel, QGroupBox, QFrame, QMdiArea, QMdiSubWindow,\
+    QLayout
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QEvent
+from PyQt5.QtCore import QEvent, QRect, QPoint, QMargins, Qt, QSize
 from math import ceil
 
 
@@ -63,3 +64,101 @@ class MobBox(QGroupBox):
         sillhouette.moveCenter(center_point)
         self.move(sillhouette.topLeft())
         return super().leaveEvent(event)
+
+class MobMdi(QMdiArea):
+    def fixGeometry(self, window, viewGeo):
+        winGeo = window.geometry()
+        if not viewGeo.contains(winGeo):
+            if winGeo.right() > viewGeo.right():
+                winGeo.moveRight(viewGeo.right())
+            if winGeo.x() < 0:
+                winGeo.moveLeft(0)
+            if winGeo != window.geometry():
+                window.setGeometry(winGeo)
+                return True
+        return False
+
+    def eventFilter(self, obj, event):
+        if (event.type() == event.Move and 
+            isinstance(obj, QMdiSubWindow) and
+            self.fixGeometry(obj, self.viewport().geometry())):
+                return True
+        return super().eventFilter(obj, event)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        viewGeo = self.viewport().geometry()
+        for win in self.subWindowList():
+            self.fixGeometry(win, viewGeo)
+
+    def order(self) -> None:
+        list = self.subWindowList()
+        print(list)
+
+
+# This class was taken from Qt Documentation
+class FlowLayout(QLayout):
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._item_list = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item: item = self.takeAt(0)
+
+    def addItem(self, item):
+        self._item_list.append(item)
+
+    def count(self):
+        return len(self._item_list)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self._item_list): return self._item_list[index]
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self._item_list): return self._item_list.pop(index)
+        return None
+
+    def expandingDirections(self): return Qt.Orientation(0)
+
+    def hasHeightForWidth(self): return True
+
+    def heightForWidth(self, width):
+        height = self._do_layout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self._do_layout(rect, False)
+
+    def sizeHint(self): return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+        for item in self._item_list:
+            size = size.expandedTo(item.minimumSize())
+        size += QSize(
+            2 * self.contentsMargins().top(), 2 * self.contentsMargins().top())
+        return size
+
+    def _do_layout(self, rect, test_only):
+        x = rect.x()
+        y = rect.y()
+        line_height = 0
+        spacing = self.spacing()
+        for item in self._item_list:
+            space_x = spacing
+            space_y = spacing
+            next_x = x + item.sizeHint().width() + space_x
+            if next_x - space_x > rect.right() and line_height > 0:
+                x = rect.x()
+                y = y + line_height + space_y
+                next_x = x + item.sizeHint().width() + space_x
+                line_height = 0
+            if not test_only:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+            x = next_x
+            line_height = max(line_height, item.sizeHint().height())
+        return y + line_height - rect.y()
--- ft/window.py
+++ ft/window.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QGroupBox, QLabel
-from PyQt5.QtGui import QPixmap
-from ft.custom_elements import MobLabel, MobBox
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QGroupBox, QLabel,\
+    QMdiArea, QFrame, QHBoxLayout, QListWidget, QListView, QScrollArea
+from PyQt5.QtGui import QPixmap, QCloseEvent
+from PyQt5.QtCore import Qt, QRect
+from ft.custom_elements import MobLabel, MobBox, MobMdi, FlowLayout
 import json
 from os.path import join
 
@@ -12,27 +14,62 @@ class MobDex(QWidget):
         self.paths = program_paths
         self.self_settings()
         self.init_gui()
+
+        #MUST DELETE
+        self.test_set3()
     
     def self_settings(self) -> None:
         
         """
-        Windows' geometry is saved each time the app is opened, reopening the
+        Window's geometry is saved each time the app is opened, reopening the
         app with the size and position from last session
         """
-        path: str = join(*self.paths.get('front').get('styling_par'))
-        with open(path, 'r') as par: setting: dict = json.load(par)
-        geometry: list = setting.get('window').get('geometry')
-        self.setGeometry(*geometry)
+        path: str = join(*self.paths.get('front').get('preset_window'))
+        with open(path, 'r') as par: self.setting: dict = json.load(par)
+        geometry: list = self.setting.get('geometry')
+        rect: QRect = QRect(*geometry)
+        self.setGeometry(rect)
 
         """
         Reads the language setted in the app, then retrieves all the data
-        containing the text in string format
+        containing the text
         """
-        setted_lang: str = setting.get('window').get('language')
-        
+        setted_lang: str = self.setting.get('language')
+        path: str = join(
+            *self.paths.get('front').get('languages').get(setted_lang))
+        with open(path, 'r') as file: self.text: dict = json.load(file)
 
+        """
+        Setting window's presentation
+        """
+        title: str = self.text.get('window_title')
+        self.setWindowTitle(title)
+
+    def test_set3(self) -> None:
+        lay = FlowLayout()
+        for i in range(286):
+            marco = MobBox(title=str(i))
+            marco.setMinimumSize(100, 120)
+            marco.resize(marco.minimumSize())
+            marco.set_base_size()
+            lay.addWidget(marco)
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        transition_w = QWidget()
+        transition_w.setLayout(lay)
+        scroll.setWidget(transition_w)
+        transition_l = QHBoxLayout()
+        transition_l.addWidget(scroll)
+        self.setLayout(transition_l)
         pass
 
     def init_gui(self) -> None:
         
         pass
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        sillhouette = self.frameGeometry().getRect()
+        self.setting['geometry'] = sillhouette
+        path: str = join(*self.paths.get('front').get('preset_window'))
+        with open(path, 'w') as par: json.dump(self.setting, par)
+        return super().closeEvent(event)
